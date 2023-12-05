51000f756bb6fe20ccec5e93af32b74edf6cc80e
Levi Ceccato <levi.ceccato@gmail.com>
2023-12-06 08:01:00 +1100

    Simplify

--- 04-scratchcards/main.ts
+++ 04-scratchcards/main.ts
@@ -11,12 +11,13 @@ console.log(`Total points: ${pointCount}`)
 
 const cardCounts = cards.map(() => 1)
 
-cards.forEach((card) => {
-	const startIndex = card.number + 1
-	const winningCards = cards.slice(startIndex, startIndex + getWinCount(card))
-	const count = cardCounts.at(card.number) || 0
-	const newCards = Array.from({ length: count }).flatMap(() => winningCards)
-	newCards.forEach((newCard) => cardCounts[newCard.number]++)
+cards.forEach((card, cardIndex) => {
+	let index = cardIndex + 1
+	const endIndex = index + getWinCount(card)
+	while (index < endIndex) {
+		cardCounts[index] += cardCounts[cardIndex]
+		index++
+	}
 })
 
 const cardCount = cardCounts.reduce((sum, c) => sum + c, 0)
@@ -24,32 +25,24 @@ const cardCount = cardCounts.reduce((sum, c) => sum + c, 0)
 console.log(`Total scratchcards: ${cardCount}`)
 
 type Card = {
-	number: number
-	winningNums: Set<number>
-	drawnNums: Set<number>
+	winning: number[]
+	drawn: number[]
 }
 
-function parseCard(rawCard: string, number: number): Card {
-	const [rawWinningNums, rawDrawnNums] =
-		rawCard.split(': ').at(-1)?.split(' | ') || []
+function parseCard(rawCard: string): Card {
+	const [rawWinning, rawDrawn] = rawCard.split(': ').at(-1)?.split(' | ') || []
 	return {
-		number,
-		winningNums: parseNums(rawWinningNums),
-		drawnNums: parseNums(rawDrawnNums),
+		winning: parseNums(rawWinning),
+		drawn: parseNums(rawDrawn),
 	}
 }
 
-function parseNums(rawNums: string): Set<number> {
-	const nums = rawNums
-		.replaceAll('  ', ' ')
-		.split(' ')
-		.filter(Boolean)
-		.map(Number)
-	return new Set(nums)
+function parseNums(rawNums: string): number[] {
+	return rawNums.replaceAll('  ', ' ').split(' ').filter(Boolean).map(Number)
 }
 
 function getWinCount(card: Card): number {
-	return Array.from(card.drawnNums).filter((num) => card.winningNums.has(num))
+	return Array.from(card.drawn).filter((num) => card.winning.includes(num))
 		.length
 }
 
