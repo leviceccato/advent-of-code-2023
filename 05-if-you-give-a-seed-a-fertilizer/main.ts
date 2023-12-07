bb4f37a33ee930a780f8c22371fc06cf8cb2a26d
Levi Ceccato <levi.ceccato@gmail.com>
2023-12-08 07:56:25 +1100

    Add part 2 solution

--- 05-if-you-give-a-seed-a-fertilizer/main.ts
+++ 05-if-you-give-a-seed-a-fertilizer/main.ts
@@ -1,35 +1,104 @@
 const input = await Bun.file('input.txt').text()
 
-const [rawSeeds, ...rawMaps] = input.split('\n\n')
+const [rawSeeds, ...rawMappingRangeGroups] = input.split('\n\n')
 
 const seeds = rawSeeds.split(': ').at(-1)?.split(' ').map(Number) || []
 
-const maps = rawMaps.map(parseMap)
+const mappingRangeGroups = rawMappingRangeGroups.map(parseMappingRange)
 
-const lowestLocation = seeds.reduce((previousValue, value) => {
-	const location = maps.reduce((r, map, i) => map(r), value)
-	return location < previousValue ? location : previousValue
-}, Infinity)
+const seedRanges = seeds.map<Range>((seed) => ({ start: seed, end: seed }))
 
-console.log(`Lowest location: ${lowestLocation}`)
+const lowestStart = processRanges(seedRanges, mappingRangeGroups)
 
-function parseMap(rawMap: string): (_: number) => number {
-	const [_, ...rawEntries] = rawMap.split('\n')
-	const entries = rawEntries.map((rawEntry) => {
-		const [destStart, sourceStart, length] = rawEntry.split(' ').map(Number)
-		return (previous: number) =>
-			previous < sourceStart || previous > sourceStart + length - 1
-				? previous
-				: destStart + (previous - sourceStart)
-	})
-	return (value: number) => {
-		let result = value
-		for (const entry of entries) {
-			result = entry(result)
-			if (result !== value) {
-				break
+const seedRanges2 = seeds.flatMap<Range>((seed, seedIndex) => {
+	return seedIndex % 2
+		? []
+		: [{ start: seed, end: seed + seeds[seedIndex + 1] - 1 }]
+})
+
+const lowestStart2 = processRanges(seedRanges2, mappingRangeGroups)
+
+console.log(`Lowest location number: ${lowestStart}
+Lowest location number adjusted: ${lowestStart2}`)
+
+type Range = {
+	start: number
+	end: number
+}
+
+type MappingRange = Range & {
+	offset: number
+}
+
+type MappingRangeResult = {
+	mapped: Range[]
+	nonMapped: Range[]
+}
+
+function processRanges(
+	ranges: Range[],
+	mappingGroups: MappingRange[][],
+): number {
+	for (const mappingRangeGroup of mappingRangeGroups) {
+		let newRanges: Range[] = []
+		for (const range of ranges) {
+			let nonMappedRanges = [range]
+			let mappedRanges: Range[] = []
+			for (const mappingRange of mappingRangeGroup) {
+				for (const nonMappedRange of nonMappedRanges) {
+					const result = mapRange(nonMappedRange, mappingRange)
+					mappedRanges = mappedRanges.concat(result.mapped)
+					nonMappedRanges = result.nonMapped
+				}
 			}
+			newRanges = newRanges.concat(mappedRanges.concat(nonMappedRanges))
 		}
+		ranges = newRanges
+	}
+	return ranges.reduce(
+		(lowestStart, range) => Math.min(lowestStart, range.start),
+		Infinity,
+	)
+}
+
+function mapRange(r: Range, mr: MappingRange): MappingRangeResult {
+	const maxStart = Math.max(r.start, mr.start)
+	const minEnd = Math.min(r.end, mr.end)
+	const result: MappingRangeResult = {
+		mapped: [],
+		nonMapped: [],
+	}
+	if (maxStart > minEnd) {
+		result.nonMapped.push(r)
 		return result
 	}
+	result.mapped.push({
+		start: maxStart + mr.offset,
+		end: minEnd + mr.offset,
+	})
+	if (r.start < mr.start) {
+		result.nonMapped.push({
+			start: r.start,
+			end: mr.start - 1,
+		})
+	}
+	if (r.end > mr.end) {
+		result.nonMapped.push({
+			start: mr.end + 1,
+			end: r.end,
+		})
+	}
+	return result
+}
+
+function parseMappingRange(rawMap: string): MappingRange[] {
+	const [_, ...rawEntries] = rawMap.split('\n')
+	return rawEntries.map((rawEntry) => {
+		const [destStart, sourceStart, length] = rawEntry.split(' ').map(Number)
+		return {
+			start: sourceStart,
+			end: sourceStart + length - 1,
+			offset: destStart - sourceStart,
+		}
+	})
 }
