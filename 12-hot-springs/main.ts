7c7d3dcf5649597cc3db3ada865683ba7656fd84
Levi Ceccato <levi.ceccato@gmail.com>
2023-12-23 14:34:29 +1100

    Add WIP parsing

--- 12-hot-springs/main.ts
+++ 12-hot-springs/main.ts
@@ -1,1 +1,38 @@
 const input = await Bun.file('input.txt').text()
+
+const springRecords = input.split('\n').map(parseSpringRecord)
+
+type SpringCondition = 'operational' | 'damaged' | 'unknown'
+
+type SpringRecord = {
+	damagedGroups: number[]
+	conditions: SpringCondition[]
+}
+
+function parseSpringRecord(rawSpringRecord: string): SpringRecord {
+	const [rawConditions, rawDamagedGroups] = rawSpringRecord.split(' ')
+	return {
+		damagedGroups: rawDamagedGroups.split(',').map(Number),
+		conditions: rawConditions.split('').map((rawCondition) => {
+			switch (rawCondition) {
+				case '.':
+					return 'operational'
+				case '#':
+					return 'damaged'
+				default:
+					return 'unknown'
+			}
+		}),
+	}
+}
+
+function getConditionCombinations(
+	springRecord: SpringRecord,
+): SpringCondition[][] {
+	const unknownGroupIndexes: number[] = []
+	springRecord.conditions.forEach((condition, conditionIndex) => {
+		if (condition === 'unknown') {
+			unknownGroupIndexes.push(conditionIndex)
+		}
+	})
+}
